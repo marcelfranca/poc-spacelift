7d7bf7c8619cb1265cec88218c9b1ff3ff700dac
Marcel França <marcel@marcelfranca.com>
2023-01-24 15:49:06 -0300

    - provider

--- aws/providers.tf
+++ aws/providers.tf
@@ -1,10 +1,10 @@
-# provider "aws" {
-#   region = "us-west-2"
-# }
+provider "aws" {
+  region = "us-west-2"
+}
 
 
-# provider "spacelift" {
-#   api_key_endpoint = "https://marcelfranca.app.spacelift.io"
-#   api_key_id       = env.SPACELIFT_API_TOKEN
-#   api_key_secret   = env.SPACELIFT_OIDC_TOKEN
-# }
+provider "spacelift" {
+  api_key_endpoint = "https://marcelfranca.app.spacelift.io"
+  api_key_id       = env.SPACELIFT_API_TOKEN
+  api_key_secret   = env.SPACELIFT_OIDC_TOKEN
+}
