b1b0a33048995486f06c9ad8b70e6eca93555f31
Marcel França <marcel@marcelfranca.com>
2023-01-24 15:47:23 -0300

    - destroy

--- aws/eips.tf
+++ aws/eips.tf
@@ -1,7 +1,7 @@
-resource "aws_eip" "nat1" {
-  depends_on = [aws_internet_gateway.dev]
-}
+# resource "aws_eip" "nat1" {
+#   depends_on = [aws_internet_gateway.dev]
+# }
 
-resource "aws_eip" "nat2" {
-  depends_on = [aws_internet_gateway.dev]
-}
+# resource "aws_eip" "nat2" {
+#   depends_on = [aws_internet_gateway.dev]
+# }
--- aws/internet-gateway.tf
+++ aws/internet-gateway.tf
@@ -1,7 +1,7 @@
-resource "aws_internet_gateway" "dev" {
-  vpc_id = aws_vpc.dev.id
+# resource "aws_internet_gateway" "dev" {
+#   vpc_id = aws_vpc.dev.id
 
-  tags = {
-    Name = "dev"
-  }
-}
+#   tags = {
+#     Name = "dev"
+#   }
+# }
